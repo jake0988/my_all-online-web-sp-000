40272df7155d2ddc05c74eb3ceb1c75b9fe0e4db
Jake G <jake098877@gmail.com>
2020-04-29 01:12:11 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,12 +1,18 @@
 require 'pry'
 
-def my_all?(collection)
-  i = 0
-  block_return_values = []
-  while i < collection.length
-    yield(collection[i])
-    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+  def my_all?(collection)
+    i = 0
+    block_return_values = []
+    while i < collection.length
+      block_return_values << yield(collection[i])
+      i = i + 1
+    end
+
+    if block_return_values.include?(false)
+      false
+    else
+      true
+    end
   end
-end
 
 my_all?([1,2,3]) {|i| i < 2}
